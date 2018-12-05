451e04e896c532806ff4aa3b52f25c23f385d676
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-12-05 15:41:53 -0600

    created knight class and edited board to return coordinates with letters

--- script.rb
+++ script.rb
@@ -1,5 +1,12 @@
+#1. Put together a script that creates a game board and a knight.
+#2. Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
+#3. Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
+#4. Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
+ 	
+
 require_relative "lib/knight.rb" 
 
 new_board = Chess_board.new
-
-new_board.board.each {|row| p row}
+new_knight = Knight.new
+new_board.board[0][0]  #=> 'a1'--> row 1 // column a
+p new_knight.position
