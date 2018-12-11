1781e8fcbb152f7eb0f239bbce818307e24cec20
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-12-11 14:22:59 -0600

    maybe ill come back to this

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,141 +1,60 @@
 
-# CLASS OBJECTS  - - - - - - - - - - - - - -
-#  - - - - - - - - - - - - - - - - - - - - -
-class Chess_board
-	attr_reader :board
+class Node
+	attr_accessor :x, :y, :parent
 
-	def initialize
-		@board = [
-          ['0','1','2','3','4','5','6','7'],#0
-          ['0','1','2','3','4','5','6','7'],#1
-          ['0','1','2','3','4','5','6','7'],#2
-          ['0','1','2','3','4','5','6','7'],#3
-          ['0','1','2','3','4','5','6','7'],#4
-          ['0','1','2','3','4','5','6','7'],#5
-          ['0','1','2','3','4','5','6','7'],#6
-          ['0','1','2','3','4','5','6','7'],#7
-		]
-	end	
+	def initialize(x,y, parent = nil)
+	@x = x
+	@y = y
+	@parent = parent
+	end 
+
+	def list_moves
+		moves = [ [@x+1, @y+2], [@x+2, @y+1], [@x+2, @y-1], [@x+1, @y-2], [@x-1, @y -2], [@x-2, @y-1], [@x-2, @y+1], [@x-1, @y+2] ]
+        moves.select { |x| x[0]>=1 && x[0]<=8 && x[1]>=1 && x[1]<=8 }.map do |coordinates|
+            Node.new(coordinates[0], coordinates[1], self)
+        end
+    end    
+
 end	
 
-class Knight
-	attr_accessor :position, :row, :column, :move_1, :move_2, :move_3, :move_4, :move_5, :move_6, :move_7, :move_8, :all_moves, :path 
+def knight_moves(start, stop)
+  
+    start_object = Node.new(start[0], start[1])
+    stop_object  = Node.new(stop[0], stop[1])
 
- 	def initialize
- 		@row    = 0
- 		@column = 0
- 		@path   = [] #array of arrays, path size == (length - 1)
- 		@move_1 = nil
- 		@move_2 = nil
- 		@move_3 = nil
- 		@move_4 = nil
- 		@move_5 = nil
- 		@move_6 = nil
- 		@move_7 = nil
- 		@move_8 = nil
- 		@all_moves = nil
+    # Get path from end location to start location
+    find_path = search(stop_object, start_object)
+    p find_path
+    # Get route of knight
+    route = []
 
-	end
-#  - - - - - - - - - - - - - - - - - - - - -
-	def position
-		return [row,column]
-	end	
-
-#  - - - - - - - - - - - - - - - - - - - - -	
-	def place_knight(coordinates)
-	  self.row    = coordinates[0]
-	  self.column = coordinates[1]	
-	end	
-
-#  - - - - - - - - - - - - - - - - - - - - -
-	def knight_moves(start_pos, end_pos)
-	  place_knight(start_pos)
-	  path << position
-	  
-	  
-
-	  if path[-1] == end_pos
-	  	p self
-	    return self
-	  else
-	   puts 'booooo'  
-
-	  end  	
-
-	  # if path.length > 63
-	  # 	puts 'test 1'
-   #      return nil
-	  # end	
-      
-   #    if self.possible_moves.include?(end_pos)
-   #    	puts 'test 2'
-   #      path << end_pos
-   #      return self
-   #    end	
-
-	  # # if start_pos == end_pos
-	  # # 	puts 'test 2'
-	  # #   path << end_pos	
-	  # #   puts "this the path -> #{path}"
-   # #      return path 
-	  # # end	
-
-	  # if self.possible_moves.include?(end_pos) == false
-	  # 	puts 'test 3'
-	  #  	self.possible_moves.each do |move|
-	  #  		return move if move == end_pos
-	  #  		knight_moves(move,end_pos)
-	  #    end	
-	  # end	
+    route.unshift([find_path.x, find_path.y])
+    current_node = find_path.parent
+    until current_node.nil?
+        route.unshift([current_node.x, current_node.y])
+        current_node = current_node.parent
     end
-
-#  - - - - - - - - - - - - - - - - - - - - -    
-    def possible_moves
     
-    	x = self.position[0] 
-    	y = self.position[1]
-    	
-    	self.move_1 = [x+2, y+1]
-    	self.move_2 = [x+1, y+2]
-    	self.move_3 = [x-1, y+2]
-    	self.move_4 = [x-2, y+1]
-    	self.move_5 = [x-2, y-1]
-    	self.move_6 = [x-1, y-2]
-    	self.move_7 = [x+1, y-2]
-    	self.move_8 = [x+2, y-1]
-    	self.all_moves = [move_1, move_2, move_3, move_4, move_5, move_6, move_7, move_8]
-    	
-    	check_possible_moves
-        all_moves
-    end	
-#  - - - - - - - - - - - - - - - - - - - - -
-    def remove_repeated_moves
-      path.each do |past_move|
-          all_moves.each do |possible_move| 
-            all_moves.delete_if {|move| move == past_move}
-          end	
-      end
-
-    end	
-#  - - - - - - - - - - - - - - - - - - - - -
-    def check_possible_moves
-      
-      self.all_moves.map! do |move|
-        if move[0] < 0 || move[1] < 0 	
-           move = nil
-        elsif move[0] > 7 || move[1] > 7
-           move = nil   	   	   
-        else
-          move
-        end     		
-      end	
-      self.all_moves.delete_if {|move| move == nil }
-      remove_repeated_moves
-    end	
+    # Print route to user
+    puts "You made it in #{route.size - 1} moves!"
+    puts "Your Path: #{route}"
+    return route
+   
 end
 
-##  - - - - - - - - - - - - - - - - - - - - -		
+def search(target, starting_location)
+    
+    queue = []
+    queue << starting_location #[0,0], target [4,4]
 
-# knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
-# knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
-# knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
+    until queue.empty?
+     
+        current_node = queue.shift
+        if current_node.x == target.x && current_node.y == target.y
+            p current_node
+            return current_node
+        else
+            current_node.list_moves.each { |child_move| queue << child_move }
+        end
+    end
+end
--- script.rb
+++ script.rb
@@ -1,23 +1,8 @@
 
 require_relative "lib/knight.rb" 
 
-new_board = Chess_board.new
-new_knight = Knight.new
+new_node = Node.new(0,0)
+target_node = Node.new(6,4)
 
-
-new_knight.knight_moves([4,4],[7,7])
-
-# - - - - - - - -  - - - - - - --
-
-
-# - - - - - - - -  - - - - - - --
-
-
-
-
- 
-
- # if knight position equals destination return path 
-
- # list of possible moves [1,2] & [2,1]
- #  MOVE the knight and track is path 
+#search(target_node,new_node)
+knight_moves([0,0],[4,4])
